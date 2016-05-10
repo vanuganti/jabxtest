ed1921b00e7f147a6a550ca8fff952171e2c19b5
Venu Anuganti <venu@venublog.com>
2016-05-10 12:12:09 -0700

    little more refactoring

--- src/main/java/com/foobar/TestJaxb.java
+++ src/main/java/com/foobar/TestJaxb.java
@@ -1,20 +1,10 @@
 package com.foobar;
 
-import java.io.File;
-
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamWriter;
-import java.io.StringWriter;
-
+import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -22,217 +12,246 @@ import java.util.concurrent.TimeUnit;
 
 public class TestJaxb {
     public static void main(String[] args) {
-     try {
-        int maxThreads = 100;
-        int maxRequests = 1000;
-        ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
-        File inputFile = new File("test.xml");
-        System.out.println("running " + maxRequests + " requests with " + maxThreads + " threads to test JAXBContext overhead");
+        try {
+            int maxThreads = 100;
+            int maxRequests = 1000;
+
+            File inputFile = new File("test.xml");
+            System.out.println("running " + maxRequests + " requests with " + maxThreads + " threads to test JAXBContext overhead");
+
+            // non singleton
+            long nonSingletonElapsedTime = runNonSingleton(maxThreads, maxRequests, inputFile);
+            System.out.println("total time: " + String.format("%05d", nonSingletonElapsedTime) + " ms (using local instance, non singleton)");
+
+            // singleton
+            long singletonElapsedTime = runSingleton(maxThreads, maxRequests, inputFile);
+            System.out.println("total time: " + String.format("%05d", singletonElapsedTime) + " ms (using global instance, singleton)");
+
+            if (singletonElapsedTime < nonSingletonElapsedTime) {
+                System.out.println("++ summary: singleton is " + String.format("%03d", nonSingletonElapsedTime / singletonElapsedTime) + " times faster than non singleton");
+            }
+            else {
+                System.out.println("++ summary: singleton is " + String.format("%03d", nonSingletonElapsedTime / singletonElapsedTime) + " times slower than non singleton");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static long runSingleton(int totalThreads, int totalRequests, File inputFile) {
+        long startTime = System.currentTimeMillis();
+        try {
+
+            JAXBContext jaxbContext = JAXBContext.newInstance(AuthenticateUserReqMessage.class);
+            ExecutorService executor = Executors.newFixedThreadPool(totalThreads);
+
+            for (int i = 0; i < totalRequests; i++) {
+                executor.submit(new SingletonRunner(i, inputFile, jaxbContext));
+            }
+
+            executor.shutdown();
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    private static long runNonSingleton(int totalThreads, int totalRequests, File inputFile) {
 
         long startTime = System.currentTimeMillis();
-
-        for (int i=0; i< maxRequests; i++) {
-            executor.submit(new RunClass(i, inputFile));
-        }
-        executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.SECONDS);
-        long estimatedTime = System.currentTimeMillis() - startTime;
-
-        System.out.println("total time for first test (using class instance in each call): " + estimatedTime + " ms");
-
-        JAXBContext jaxbContext = JAXBContext.newInstance(AuthenticateUserReqMessage.class);
-        executor = Executors.newFixedThreadPool(maxThreads);
-
-        startTime = System.currentTimeMillis();
-
-        for (int i=0; i< maxRequests; i++) {
-            executor.submit(new RunClass2(i, inputFile, jaxbContext));
-        }
-        executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.SECONDS);
-        estimatedTime = System.currentTimeMillis() - startTime;
-
-        System.out.println("total time for second test(using global class instance): " + estimatedTime + " ms");
-
-
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-
-public static class RunClass implements Runnable {
-
-    int threadId = 0;
-    File inputFile = null;
-
-    public RunClass(int id, File inputFile) {
-        this.threadId = id;
-        this.inputFile = inputFile;
-    }
-    public void run() {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(AuthenticateUserReqMessage.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            AuthenticateUserReqMessage authResponse = (AuthenticateUserReqMessage) jaxbUnmarshaller.unmarshal(inputFile); 
-            if (!authResponse.getChannelPartnerID().equals("testChannel")) {
-                System.out.println("[" + threadId + "] failed in RunClass, response : " + authResponse.getChannelPartnerID());
+
+            ExecutorService executor = Executors.newFixedThreadPool(totalThreads);
+
+            for (int i = 0; i < totalRequests; i++) {
+                executor.submit(new RunnerNonSingleton(i, inputFile));
             }
-        } catch (Exception e) {
+
+            executor.shutdown();
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        } catch(Exception e) {
             e.printStackTrace();
         }
+        return System.currentTimeMillis() - startTime;
     }
-}
 
-public static class RunClass2 implements Runnable {
+    public static class SingletonRunner implements Runnable {
 
-    int threadId = 0;
-    File inputFile = null;
-    JAXBContext jaxbContext = null;
-    Unmarshaller jaxbUnmarshaller;
+        int threadId = 0;
+        File inputFile = null;
+        JAXBContext jaxbContext = null;
 
-    public RunClass2(int id, File inputFile, JAXBContext jaxbContext) {
-        this.threadId = id;
-        this.inputFile = inputFile;
-        this.jaxbContext = jaxbContext;
-        try {
-            jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        } catch (Exception e){e.printStackTrace();}
-    }
-    public void run() {
-        try {
-            AuthenticateUserReqMessage authResponse = (AuthenticateUserReqMessage) jaxbUnmarshaller.unmarshal(inputFile); 
-            if (!authResponse.getChannelPartnerID().equals("testChannel")) {
-                System.out.println("[" + threadId + "] failed in RunClass2, response : " + authResponse.getChannelPartnerID());
+        public SingletonRunner(int id, File inputFile, JAXBContext jaxbContext) {
+            this.threadId = id;
+            this.inputFile = inputFile;
+            this.jaxbContext = jaxbContext;
+        }
+
+        public void run() {
+            try {
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                AuthenticateUserReqMessage authResponse = (AuthenticateUserReqMessage) jaxbUnmarshaller.unmarshal(inputFile);
+                if (!authResponse.getChannelPartnerID().equals("testChannel")) {
+                    System.out.println("[" + threadId + "] failed in SingletonRunner, response : " + authResponse.getChannelPartnerID());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
-}
 
-@XmlRootElement(name = "AuthenticateUserReqMessage")
-public static class AuthenticateUserReqMessage {
 
-    private String apiUser;
-    private String apiPassword;
-    private String contactID;
-    private String message;
-    private String customerUsername;
-    private String customerPassword;    
-    private Boolean generateToken;
-    private String socialLoginType;
-    private String socialLoginID;
-    private Boolean rememberMe;
-    private Boolean isGlobalSearch;
-    private String channelPartnerID;
+    public static class RunnerNonSingleton implements Runnable {
 
-    @XmlElement(name = "apiUser")
-    public String getApiUser() {
-        return apiUser;
+        int threadId = 0;
+        File inputFile = null;
+
+        public RunnerNonSingleton(int id, File inputFile) {
+            this.threadId = id;
+            this.inputFile = inputFile;
+        }
+
+        public void run() {
+            try {
+                JAXBContext jaxbContext = JAXBContext.newInstance(AuthenticateUserReqMessage.class);
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                AuthenticateUserReqMessage authResponse = (AuthenticateUserReqMessage) jaxbUnmarshaller.unmarshal(inputFile);
+                if (!authResponse.getChannelPartnerID().equals("testChannel")) {
+                    System.out.println("[" + threadId + "] failed in RunnerNonSingleton, response : " + authResponse.getChannelPartnerID());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void setApiUser(String apiUser) {
-        this.apiUser = apiUser;
-    }
+    @SuppressWarnings("unused")
+    @XmlRootElement(name = "AuthenticateUserReqMessage")
+    public static class AuthenticateUserReqMessage {
 
-    @XmlElement(name = "apiPassword")
-    public String getApiPassword() {
-        return apiPassword;
-    }
+        private String apiUser;
+        private String apiPassword;
+        private String contactID;
+        private String message;
+        private String customerUsername;
+        private String customerPassword;
+        private Boolean generateToken;
+        private String socialLoginType;
+        private String socialLoginID;
+        private Boolean rememberMe;
+        private Boolean isGlobalSearch;
+        private String channelPartnerID;
 
-    public void setApiPassword(String apiPassword) {
-        this.apiPassword = apiPassword;
-    }
+        @XmlElement(name = "apiUser")
+        public String getApiUser() {
+            return apiUser;
+        }
 
-    @XmlElement(name = "customerPassword")
-    public String getCustomerPassword() {
-        return customerPassword;
-    }
+        public void setApiUser(String apiUser) {
+            this.apiUser = apiUser;
+        }
 
-    public void setCustomerPassword(String customerPassword) {
-        this.customerPassword = customerPassword;
-    }
+        @XmlElement(name = "apiPassword")
+        public String getApiPassword() {
+            return apiPassword;
+        }
 
-    @XmlElement(name = "customerUsername")
-    public String getCustomerUsername() {
-        return customerUsername;
-    }
+        public void setApiPassword(String apiPassword) {
+            this.apiPassword = apiPassword;
+        }
 
-    public void setCustomerUsername(String customerUsername) {
-        this.customerUsername = customerUsername;
-    }
+        @XmlElement(name = "customerPassword")
+        public String getCustomerPassword() {
+            return customerPassword;
+        }
 
-    @XmlElement(name = "generateToken")
-    public Boolean getGenerateToken() {
-        return generateToken;
-    }
+        public void setCustomerPassword(String customerPassword) {
+            this.customerPassword = customerPassword;
+        }
 
-    public void setGenerateToken(Boolean generateToken) {
-        this.generateToken = generateToken;
-    }
+        @XmlElement(name = "customerUsername")
+        public String getCustomerUsername() {
+            return customerUsername;
+        }
 
-    @XmlElement(name = "socialLoginType")
-    public String getSocialLoginType() {
-        return socialLoginType;
-    }
+        public void setCustomerUsername(String customerUsername) {
+            this.customerUsername = customerUsername;
+        }
 
-    public void setSocialLoginType(String socialLoginType) {
-        this.socialLoginType = socialLoginType;
-    }
+        @XmlElement(name = "generateToken")
+        public Boolean getGenerateToken() {
+            return generateToken;
+        }
 
-    @XmlElement(name = "socialLoginID")
-    public String getSocialLoginID() {
-        return socialLoginID;
-    }
+        public void setGenerateToken(Boolean generateToken) {
+            this.generateToken = generateToken;
+        }
 
-    public void setSocialLoginID(String socialLoginID) {
-        this.socialLoginID = socialLoginID;
-    }
+        @XmlElement(name = "socialLoginType")
+        public String getSocialLoginType() {
+            return socialLoginType;
+        }
 
-    @XmlElement(name = "rememberMe")
-    public Boolean getRememberMe() {
-        return rememberMe;
-    }
+        public void setSocialLoginType(String socialLoginType) {
+            this.socialLoginType = socialLoginType;
+        }
 
-    public void setRememberMe(Boolean rememberMe) {
-        this.rememberMe = rememberMe;
-    }
+        @XmlElement(name = "socialLoginID")
+        public String getSocialLoginID() {
+            return socialLoginID;
+        }
 
-    @XmlElement(name = "isGlobalSearch")
-    public Boolean getIsGlobalSearch() {
-        return isGlobalSearch;
-    }
+        public void setSocialLoginID(String socialLoginID) {
+            this.socialLoginID = socialLoginID;
+        }
 
-    public void setIsGlobalSearch(Boolean isGlobalSearch) {
-        this.isGlobalSearch = isGlobalSearch;
-    }
+        @XmlElement(name = "rememberMe")
+        public Boolean getRememberMe() {
+            return rememberMe;
+        }
 
-    @XmlElement(name = "channelPartnerID")
-    public String getChannelPartnerID() {
-        return channelPartnerID;
-    }
+        public void setRememberMe(Boolean rememberMe) {
+            this.rememberMe = rememberMe;
+        }
 
-    public void setChannelPartnerID(String channelPartnerID) {
-        this.channelPartnerID = channelPartnerID;
-    }
+        @XmlElement(name = "isGlobalSearch")
+        public Boolean getIsGlobalSearch() {
+            return isGlobalSearch;
+        }
 
-    @XmlElement(name = "contactID")
-    public String getContactID() {
-        return contactID;
-    }
+        public void setIsGlobalSearch(Boolean isGlobalSearch) {
+            this.isGlobalSearch = isGlobalSearch;
+        }
 
-    public void setContactID(String contactID) {
-        this.contactID = contactID;
-    }
+        @XmlElement(name = "channelPartnerID")
+        public String getChannelPartnerID() {
+            return channelPartnerID;
+        }
 
-    @XmlElement(name = "message")
-    public String getMessage() {
-        return message;
-    }
+        public void setChannelPartnerID(String channelPartnerID) {
+            this.channelPartnerID = channelPartnerID;
+        }
 
-    public void setMessage(String message) {
-        this.message = message;
+        @XmlElement(name = "contactID")
+        public String getContactID() {
+            return contactID;
+        }
+
+        public void setContactID(String contactID) {
+            this.contactID = contactID;
+        }
+
+        @XmlElement(name = "message")
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
-}
 }
 
